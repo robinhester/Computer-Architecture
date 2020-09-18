afaa77daafef8d092229fd5319db5506c2949719
Robin Hester <robin.hester@yahoo.com>
2020-09-18 13:11:56 -0400

    MVP REACHED
    passed tests

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,28 +7,69 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.PC = 0
+        self.IR = None
+        self.FL = None
+        self.MAR = None
+        self.MDR = None
+        self.ram = [None] * 256
+        self.reg = [0] * 8
+        self.running = True
+        self.sp = 7
+
+        LDI = 130
+        MUL = 162
+        PRN = 71
+        HLT = 1
+        PUSH = 69
+        POP = 70
+        ADD = 160
+        CALL = 80
+        RET = 17
+        CMP = 167
+        JEQ = 85
+        JNE = 86
+        JMP = 84
+
+        self.branchtable = {}
+        self.branchtable[LDI] = self.ldi
+        self.branchtable[ADD] = self.add 
+        self.branchtable[MUL] = self.mul 
+        self.branchtable[PRN] = self.prn 
+        self.branchtable[HLT] = self.hlt 
+        self.branchtable[PUSH] = self.push 
+        self.branchtable[POP] = self.pop 
+        self.branchtable[CALL] = self.call 
+        self.branchtable[RET] = self.ret 
+        self.branchtable[CMP] = self.cmp 
+        self.branchtable[JEQ] = self.jeq 
+        self.branchtable[JNE] = self.jne 
+        self.branchtable[JMP] = self.jmp 
 
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        if len(sys.argv) != 2:
+            print("usage: file ls8.py")
+            sys.exit(1)
 
-        # For now, we've just hardcoded a program:
+        filename = sys.argv[1];
+        try:
+            address = 0
+            with open(filename) as f:
+                for line in f:
+                    comment_split = line.split("#")
+                    num = comment_split[0].strip()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    if num == '':
+                        continue
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    val = int(num, 2)
+                    self.ram[address] = val
+                    address += 1
+        except FileNotFoundError:
+            print("File not found")
+            sys.exit(1)
 
 
     def alu(self, op, reg_a, reg_b):
@@ -36,7 +77,30 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            self.FL = 0x00
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.FL = self.FL | 0b00000001
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.FL = self.FL | 0b00000100
+            if self.reg[reg_a] > self.reg[reg_b]:
+                self.FL = self.FL | 0b00000010
+        elif op == "ADD":
+            self.reg[reg_a] = self.reg[reg_a] & self.reg[reg_b]
+        elif op == "OR":
+            self.reg[reg_a] = self.reg[reg_a] | self.reg[reg_b]
+        elif op == "XOR":
+            self.reg[reg_a] = self.reg[reg_a] ^ self.reg[reg_b]
+        elif op == "NOT":
+            self.reg[reg_a] = ~self.reg[reg_a]
+        elif op == "SHL":
+            self.reg[reg_a] = self.reg[reg_a] << self.reg[reg_b]
+        elif op == "SHR":
+            self.reg[reg_a] = self.reg[reg_a] >> self.reg[reg_b]
+        elif op == "MOD":
+            self.reg[reg_a] = self.reg[reg_a] % self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -47,12 +111,10 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.PC,
+            self.ram_read(self.PC),
+            self.ram_read(self.PC + 1),
+            self.ram_read(self.PC + 2)
         ), end='')
 
         for i in range(8):
@@ -62,4 +124,94 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while self.running:
+            self.IR = self.ram_read(self.PC)
+            self.branchtable[self.IR]()
+
+    def ram_read(self, address):
+        self.MAR = address
+        self.MDR = self.ram[self.MAR]
+        return self.MDR
+
+    def ram_write(self, address, value):
+        self.MAR = address
+        self.MDR = value
+        self.ram[self.MAR] = self.MDR
+
+    def ldi(self):
+        operanda = self.ram_read(self.PC+1)
+        opperandb = self.ram_read(self.PC+2)
+        self.reg[operanda] = opperandb
+        self.PC += 3
+
+    def add(self):
+        opperanda = self.ram_read(self.PC+1)
+        opperandb = self.ram_read(self.PC+2)
+        self.alu('ADD', opperanda, opperandb)
+        self.PC += 3
+
+    def mul(self):
+        opperanda = self.ram_read(self.PC+1)
+        opperandb = self.ram_read(self.PC+2)
+        self.alu('MUL', opperanda, opperandb)
+        self.PC += 3
+
+    def prn(self):
+        opperanda = self.ram_read(self.PC+1)
+        print(self.reg[opperanda])
+        self.PC += 2
+
+    def hlt(self):
+        self.running = False
+
+    def push(self):
+        register = self.ram_read(self.PC+1)
+        value = self.reg[register]
+        self.reg[self.sp] -= 1
+        self.ram[self.reg[self.sp]] = value
+        self.PC += 2
+
+    def pop(self):
+        register = self.ram_read(self.PC+1)
+        value = self.ram_read(self.reg[self.sp])
+        self.reg[self.sp] += 1
+        self.reg[register] = value
+        self.PC += 2
+
+    def call(self):
+        register = self.ram_read(self.PC+1)
+        self.reg[self.sp] -= 1
+        self.ram_write(self.reg[self.sp], self.PC + 2)
+        self.PC = self.reg[register]
+
+    def ret(self):
+        self.PC = self.ram_read(self.reg[self.sp])
+        self.reg[self.sp] += 1
+
+    def cmp(self):
+        register_1 = self.ram_read(self.PC+1)
+        register_2 = self.ram_read(self.PC+2)
+        self.alu('CMP', register_1, register_2)
+        self.PC += 3
+
+    def jmp(self):
+        register_1 = self.ram_read(self.PC+1)
+        opperanda = self.reg[register_1]
+        self.PC = opperanda
+
+    def jeq(self):
+        register_1 = self.ram_read(self.PC+1)
+        operanda = self.reg[register_1]
+        if self.FL == 1:
+            self.PC = operanda
+        else:
+            self.PC += 2
+
+    def jne(self):
+        register_1 = self.ram_read(self.PC+1)
+        operanda = self.reg[register_1]
+        if self.FL is not 1:
+            self.PC = operanda
+        else:
+            self.PC += 2
+            
